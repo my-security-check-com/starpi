f07ad27983f3eb8cc2049fea951a19b4121106f1
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2021-08-24 11:24:20 +0200

    move database, added categories, renamed collection, added fields

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'sqlite',
-        filename: env('DATABASE_FILENAME', 'my-security-check.db'),
+        filename: env('DATABASE_FILENAME', '../my-security-check.db'),
       },
       options: {
         useNullAsDefault: true,
